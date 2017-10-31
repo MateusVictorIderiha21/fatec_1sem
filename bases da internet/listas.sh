51e43705323af6494f5640ac0d5ff4a01c6c2e66
2650831723021 <2650831723021@LAB-140.lab.fatecsaoroque.edu.br>
2017-10-31 19:28:35 -0200

    add form

--- bases da internet/listas.sh
+++ bases da internet/listas.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 iniciarLista() {
-	echo "Você deseja criar uma lista de comidas ou bebidas ou carros ou frutas? S/N";
-	read desejaContinuar
-	if [ $desejaContinuar == "S" ]; then
+	echo "Você deseja continuar para criar uma lista de comidas ou bebidas ou carros ou frutas? (S/N)";
+	read DESEJA_CONTINUAR
+	if [ $DESEJA_CONTINUAR == "S" ] || [ $DESEJA_CONTINUAR == "s" ]; then
 		criarLista;
 	else
 		exit;
@@ -15,27 +15,60 @@ criarLista() {
 	echo "2 - Bebidas";
 	echo "3 - Carro";
 	echo "4 - Frutas";
-	echo "Digite o código ou o nome da lista a ser criada 1 / 2 / 3 / 4 ?" ;
-	read tipoLista;
-	lista[];
-	if [ $tipoLista == 1 ] || [ $tipoLista == "Comidas" ]; then
-		lista=("Pastel", "Pizza", "Lazanha", "Cachorrão", "fundi", "Macarrão", "Coxinha", "Bife a milanesa", "Batata frita", "Bolacha");
-	elif [ $tipoLista == 2 ] || [ $tipoLista == "Bebidas" ]; then
-		lista=("Suco de Laranja", "Sprite", "Guarana Antartica", "wisk", "vodka", "pinga", "cerveja", "yakut", "leite", "Danone");
-	elif [ $tipoLista == 3 ] || [ $tipoLista == "Carro" ];	then
-		lista=("Gol", "pallio", "Camaro", "ferrari", "lamburguine", "mustang", "porch", "doge", "saveiro", "fusca");
-	elif [ $tipoLista == 4 ] || [ $tipoLista == "Frutas" ]; then
-		lista=("Maça","Pera","Uva","Laranja","Pessego","Melancia","Abacaxi","Abacate","Ameixa","Damasco");
+	echo "Digite o código ou o nome da lista a ser criada ( 1 / 2 / 3 / 4 ) ?";
+	read TIPO_LISTA;
+	
+	if [ $TIPO_LISTA == 1 ] || [ $TIPO_LISTA == "Comidas" ]; then
+		nomeLista="Comidas";
+		lista=("Pastel" "Pizza" "Lazanha" "Cachorrão" "fundi" "Macarrão" "Coxinha" "Bife a milanesa" "Batata frita" "Bolacha");
+	elif [ $TIPO_LISTA == 2 ] || [ $TIPO_LISTA == "Bebidas" ]; then
+		nomeLista="Bebidas";
+		lista=("Suco de Laranja" "Sprite" "Guarana Antartica" "wisk" "vodka" "pinga" "cerveja" "yakut" "leite" "Danone");
+	elif [ $TIPO_LISTA == 3 ] || [ $TIPO_LISTA == "Carro" ];	then
+		nomeLista="Carros";
+		lista=("Gol" "pallio" "Camaro" "ferrari" "lamburguine" "mustang" "porch" "doge" "saveiro" "fusca");
+	elif [ $TIPO_LISTA == 4 ] || [ $TIPO_LISTA == "Frutas" ]; then
+		nomeLista="Frutas";
+		lista=("Maça""Pera""Uva""Laranja""Pessego""Melancia""Abacaxi""Abacate""Ameixa""Damasco");
 	else
-		echo "Voçê digitou uma opção invalida, favor Selecionar uma opção valida";
+		echo "Voçê digitou uma opção invalida favor Selecionar uma opção valida";
 		criarLista;
 	fi
 
-	mostrarLista lista;
+	salvarArquivo nomeLista lista;
+	
+	iniciarLista;
 }
 
-mostrarLista() {
-	for item in $lista[*]; do 
-		echo "$item";
+salvarArquivo(){
+	indice=1;
+	GUARDAR_ARQUIVO="Lista de ${nomeLista}";
+	for item in "${lista[@]}"; do 
+		GUARDAR_ARQUIVO=("${GUARDAR_ARQUIVO} \n ${indice} - ${item}");
+		((indice++));
 	done;
+
+   	NOME_ARQUIVO_FULL="listas"
+    	N_LISTA=1;
+	OK=0;
+	while [ $OK == 0 ]; do
+	    if [ $N_LISTA > 1 ]; then
+	        NOME_ARQUIVO_FULL="listas-${N_LISTA}.txt";
+	    else
+	        NOME_ARQUIVO_FULL="listas.txt";
+	    fi
+
+		if [ -e $NOME_ARQUIVO_FULL ]; then
+			OK=0;
+		else
+			OK=1;
+		fi
+		
+		N_LISTA=$(($N_LISTA+1));
+	done;
+	
+	echo -e $GUARDAR_ARQUIVO > $NOME_ARQUIVO_FULL;
+	echo "Arquivo ${NOME_ARQUIVO_FULL} criado com sucesso!";
 }
+
+iniciarLista
